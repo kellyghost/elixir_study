d482578b1bc116b125c1f1d9fb54caa66a9f4402
your_name <your_email>
2018-11-13 16:48:49 +0800

    清空进程邮箱中所有信息，flush()

--- spawning.exs
+++ spawning.exs
@@ -13,3 +13,12 @@ spawn fn -> send(parent, {:hello,self()}) end
 receive do
   {:hello,pid} -> "Got hello from #{inspect pid}"
 end
+
+
+# 在shell中执行程序时，辅助函数flush/0很有用。它清空并打印进程邮箱中的所有消息：
+#
+# iex> send self(), :hello
+# :hello
+# iex> flush()
+# :hello
+# :ok
