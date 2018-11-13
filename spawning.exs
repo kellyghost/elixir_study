177ab464a21ca3f930556adba6c38b3ca0cb88e5
your_name <your_email>
2018-11-13 16:47:39 +0800

    两个进程间通信

--- spawning.exs
+++ spawning.exs
@@ -3,3 +3,13 @@ pid  = spawn fn -> 1 + 2 end
 IO.puts(Process.alive?(pid) )
 
 IO.puts(Process.alive?(self()) )
+
+
+# 进程间通信,可以在iex中运行
+
+parent = self()
+spawn fn -> send(parent, {:hello,self()}) end
+
+receive do
+  {:hello,pid} -> "Got hello from #{inspect pid}"
+end
