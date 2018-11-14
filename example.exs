e74c0b9d16ad73fd19e30da5d665f0f89644c73b
your_name <your_email>
2018-11-14 15:49:10 +0800

    利用Process.flag来捕捉进程的错误退出

--- example.exs
+++ example.exs
@@ -1,15 +1,15 @@
-defmodule Example do
-
-  def listen do
-    receive do
-      {:ok, "hello"} -> IO.puts("world")
-    end
-
-    listen
-
-  end
-
-end
+# defmodule Example do
+#
+#   def listen do
+#     receive do
+#       {:ok, "hello"} -> IO.puts("world")
+#     end
+#
+#     listen
+#
+#   end
+#
+# end
 
 
 
@@ -22,3 +22,19 @@ end
 #
 # iex> send pid, :ok
 # :ok
+
+
+
+
+defmodule Example do
+  def explode, do: exit(:kaboom)
+
+  def run do
+    Process.flag(:trap_exit, true)
+    spawn_link(Example, :explode, [])
+
+    receive do
+      {:EXIT, from_pid, reason} -> IO.puts("Exit reason:#{reason}")
+    end
+  end   
+end
